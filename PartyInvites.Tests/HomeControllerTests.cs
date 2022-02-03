a36438ec66c962eda76ce9e6bffeb547119b4d9b
Anton <anton.gusevv.01@gmail.com>
2022-02-03 23:48:18 +0300

    module tests using Moq objects

--- PartyInvites.Tests/HomeControllerTests.cs
+++ PartyInvites.Tests/HomeControllerTests.cs
@@ -1,36 +1,27 @@
 ﻿
 //модульный тест проверяет что метод действия ListResponses передает представлению все объекты в хранилище
-//также применена изоляция контроллера в модульном тесте
+//также применена изоляция контроллера в модульном тесте 
+//здесь не используется Moq технология
 
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Controllers;
 using PartyInvites.Models;
 using System.Collections.Generic;
 using Xunit;
+using Moq;
 
 namespace PartyInvites.Tests
 {
     public class HomeControllerTests
-    {
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<GuestResponse> Responses { get; } = new List<GuestResponse>()
-            {
-                new GuestResponse{Name = "Ann", Email = "example@email.com", PhoneNumber = "123", WillAttend = true },
-                new GuestResponse{Name = "Anton", Email = "exampleNew@email.com", PhoneNumber = "312", WillAttend = true }
-            };
-            public void AddResponse(GuestResponse response)
-            {
-                //не требуется для тестирования
-            }
-        }
-
+    {      
         [Fact]
         public void ListResponsesActionModelIsComplete()
         {
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Responses).
+                Returns(new List<GuestResponse> { new GuestResponse { Name = "Anton", Email = "exampleNew@email.com", PhoneNumber = "312", WillAttend = true } });
 
-            var controller = new HomeController();
-            controller.repository = new ModelCompleteFakeRepository();
+            var controller = new HomeController() { repository = mock.Object};
 
             var model = controller.ListResponses()?.ViewData.Model as IEnumerable<GuestResponse>;
 
@@ -39,33 +30,18 @@ namespace PartyInvites.Tests
 
 
         //тестирование дублирования запросов к хранилищу
-        class PropertyOnceFakeRepository : IRepository
-        {
-            public int PropertyCounter { get; set; } = 0;
-            public IEnumerable<GuestResponse> Responses
-            {
-                get
-                {
-                    PropertyCounter++;
-                    return new List<GuestResponse> { new GuestResponse { Name = "Anton", Email = "exampleNew@email.com", PhoneNumber = "312", WillAttend = true } };
-                }
-            }
-
-            public void AddResponse(GuestResponse response)
-            {
-                //не требуется для тестирования
-            }
-        }
 
         [Fact]
         public void RepositoryPropCalledOnce()
         {
-            var repos = new PropertyOnceFakeRepository();
-            var controller = new HomeController() { repository = repos };
+            var mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Responses)
+                .Returns(new List<GuestResponse> { new GuestResponse { Name = "Anton", Email = "exampleNew@email.com", PhoneNumber = "312", WillAttend = true } });
+            var controller = new HomeController() { repository = mock.Object };
 
             var result = controller.ListResponses();
 
-            Assert.Equal(1, repos.PropertyCounter);
+            mock.Verify(m => m.Responses, Times.Once);
 ;
         }
     }
