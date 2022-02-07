6a81d6c3a1e7f96f56757799d3cddcba1f757267
Anton <anton.gusevv.01@gmail.com>
2022-02-07 17:12:45 +0300

    add admin Index view and some tests

---
+++ Store.Tests/AdminControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Store.Controllers;
+using Store.Models;
+using Store.Models.Repositories.Abstract;
+using Xunit;
+
+namespace Store.Tests
+{
+    public class AdminControllerTests
+    {
+        /// <summary>
+        /// тестирование метода действия Index(), проверка корректного возврата объектов Product из хранилища
+        /// </summary>
+        [Fact]
+        public void IndexContainsAllProducts()
+        {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product
+                {
+                    ProductID = new Guid("054907fb-4ac2-4772-ac91-b5bf4f1fca60"),
+                    Name = "P1"
+                },
+                new Product
+                {
+                    ProductID = new Guid("17bc93a9-ae2c-461c-9147-8e017e10aa7f"),
+                    Name = "P2"
+                },
+                new Product
+                {
+                    ProductID = new Guid("2245a2f8-c292-42d8-8914-ae74ee782a72"),
+                    Name = "P3"
+                }
+            }).AsQueryable());
+            var target = new AdminController(mock.Object);
+
+            Product[] result = GetViewModel<IEnumerable<Product>>(target.Index())?.ToArray();
+
+            Assert.Equal(3, result.Length);
+            Assert.Equal("P1", result[0].Name);
+            Assert.Equal("P2", result[1].Name);
+            Assert.Equal("P3", result[2].Name);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+    }
+}
---
+++ Store/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Models.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult Index() => View(_repository.Products);
+    }
+}
